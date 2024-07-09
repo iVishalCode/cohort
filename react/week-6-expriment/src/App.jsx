5e962376522dd98670ebc5d40b87215dbb82d096
1VishalSD <ilearnvk@gmail.com>
2024-07-10 04:29:11 +0530

    test

--- react/week-6-expriment/src/App.jsx
+++ react/week-6-expriment/src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 function App() {
+  const { title, setTitle } = useState("My name is Vishal");
+
+  function updateTitle() {
+    setTitle("My name is " + Math.random());
+  }
   return (
-    <>
+    <Fragment>
+      <button onClick={updateTitle}>Update the title</button>
+      <header title={title}> Vishal</header>
       <header title="Vishal"> Vishal</header>
-      <header title="Vishal"> Vishal</header>
-      <header title="Vishal"> Vishal</header>
-      <header title="Vishal">vishal</header>
-    </>
+    </Fragment>
   );
 }
 function Header({ title }) {
