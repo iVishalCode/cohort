85b74fe8d7e9693d42bcaf55320efdead16c408a
1VishalSD <ilearnvk@gmail.com>
2024-07-14 06:07:59 +0530

    text

--- react/react-week-6.2/src/App.jsx
+++ react/react-week-6.2/src/App.jsx
@@ -1,9 +1,30 @@
 import { useEffect, useState } from "react";
-
+import axios from "axios";
 function App() {
   const [todos, setTodos] = useState(0);
-  useEffect(() => {}, []);
-  return <></>;
+  useEffect(() => {
+    axios
+      .get("https://sum-server.100xdevs.com/todos")
+      .then(function (response) {
+        setTodos(response.data.todos);
+      });
+  }, []);
+  return (
+    <>
+      (todos.map(todo=>
+      <Todo title={todos.title} descripation={todos.descripation} />
+      ))
+    </>
+  );
+}
+
+function Todo({ title, descripation }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      {descripation}
+    </div>
+  );
 }
 
 export default App;
