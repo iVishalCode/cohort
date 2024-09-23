1de59870139f4da0d607b85e9d29f5cf479d700b
1VishalSD <ilearnvk@gmail.com>
2024-09-23 11:40:31 +0530

    middleware

--- week-3/middleware/index.js
+++ week-3/middleware/index.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -6,35 +7,43 @@ app.get("/health-checkup", (req, res) => {
   const username = req.header.username;
   const password = req.header.password;
   const kidneyId = req.query.kidneyId;
-
+  
   if (username != " admin " || password != "admin") {
     res.status(400).json({
       msd: "simething up with your input ",
     });
     return;
-  }
+    }
   if (kidneyId != 1 && kidneyId != 2) {
     res.status(400).json({
       msd: "simething up with your input ",
-    });
+      });
     return;
-  }
+    }
+    res.json({
+      msg: "Your kidney is fine",
+      });
+      });
+      */
+/* with middleware
+let numberOfRequests = 0;
+function calculateRequst(req, res, next) {
+  numberOfRequests++;
+  console.log(numberOfRequests);
+  next();
+}
+app.use(calculateRequst);
+app.use(express.json());
+app.get("/health-checkup", (req, res, next) => {
   res.json({
-    msg: "Your kidney is fine",
+    msg: "hi there",
   });
 });
+app.get("/health-checkup2", (req, res) => {
+  res,
+    json({
+      msg: "hi there",
+    });
+});
 */
-
-app.get(
-  "/health-checkup",
-  (req, res, next) => {
-    console.log(" hi from req1");
-  },
-  (req, res, next) => {
-    console.log("hi from req2");
-  },
-  (req, res) => {
-    console.log("hi from req3");
-  }
-);
 app.listen(3000);
