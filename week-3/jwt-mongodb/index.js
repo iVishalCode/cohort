f2122f594490ea10991a4471dabe3c7673cbf469
1VishalSD <ilearnvk@gmail.com>
2024-09-24 10:20:59 +0530

    write a logic of jwt

--- week-3/jwt-mongodb/index.js
+++ week-3/jwt-mongodb/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-
+app.use(express.json());
 const jwtPassword = "123456";
 
 const ALL_USERS = [
@@ -22,4 +22,37 @@ const ALL_USERS = [
   },
 ];
 
-function useerExists(username, password) {}
+function userExists(username, password) {
+  let userExists = false;
+
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (
+      ALL_USERS[i].username == username &&
+      ALL_USERS[i].password == password
+    ) {
+      userExists = true;
+    }
+  }
+  return userExists;
+}
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User does not exists in our in memory db",
+    });
+  }
+
+  var token = jwt.sign(
+    {
+      username: username,
+    },
+    jwtPassword
+  );
+  return res.json({
+    token,
+  });
+});
