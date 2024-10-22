52d4a701002926c30a7f5ff2aa45702ef9646d8b
1VishalSD <ilearnvk@gmail.com>
2024-10-22 15:43:12 +0530

    done

--- week-10/dist/index.js
+++ week-10/dist/index.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log(" hello world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const client = new pg_1.Client({
+    connectionString: "postgresql://neondb_owner:WRw7x2tUzBKg@ep-silent-wildflower-a82803nr.eastus2.azure.neon.tech/neondb?sslmode=require",
+});
+try {
+    function creatUserTable() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield client.connect();
+            const result = yield client.query(`
+      CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(50) UNIQUE NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );
+  `);
+            console.log(result);
+        });
+    }
+    creatUserTable();
+}
+catch (error) {
+    console.log(" your database is not connected" + error);
+}
