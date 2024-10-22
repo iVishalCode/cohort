4f2e03ff850d34b80b334438c2e8b200957a9d6a
1VishalSD <ilearnvk@gmail.com>
2024-10-22 11:15:32 +0530

    try first query

--- week-10/src/index.ts
+++ week-10/src/index.ts
@@ -5,4 +5,17 @@ const client = new Client({
     "postgresql://neondb_owner:WRw7x2tUzBKg@ep-silent-wildflower-a82803nr.eastus2.azure.neon.tech/neondb?sslmode=require",
 });
 
-client.connect();
+async function creatUserTable() {
+  await client.connect();
+  const result = await client.query(`
+    CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+`);
+  console.log(result);
+}
+creatUserTable();
