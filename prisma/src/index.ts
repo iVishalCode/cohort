eae3835a88ac0032602565db3d7273398afda1e3
1VishalSD <ilearnvk@gmail.com>
2024-10-26 09:26:38 +0530

    prisma

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -1,1 +1,23 @@
-import mongoose from " mongoose";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  prisma.user.create({
+    data: {
+      email: username,
+      password,
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      password: true,
+    },
+  });
+}
